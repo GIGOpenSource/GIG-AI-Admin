bdd7e446fbeaeb8c33f416dd34a4ad05488be445
wyz <wyz@gig.com>
2025-09-10 09:46:50 +0800

    aids 暗色样式

--- src/components/layout/header/HeaderLogo.vue
+++ src/components/layout/header/HeaderLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link to="/" class="lg:hidden">
-    <div class="text-2xl font-bold">AiDS</div>
+    <div class="text-2xl font-bold dark:text-white" >AiDS</div>
   </router-link>
 </template>
 
