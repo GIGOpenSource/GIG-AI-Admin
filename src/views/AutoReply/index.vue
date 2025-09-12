b7b61d78c720cbd2d893dc85d4f3148713450ed6
daijiaying1 <1773292109@qq.com>
2025-09-12 13:54:50 +0800

    修改

--- src/views/AutoReply/index.vue
+++ src/views/AutoReply/index.vue
@@ -173,11 +173,11 @@
               <!-- 关注任务专用字段 -->
               <div v-if="form.type === 'follow'" class="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <div class="sm:col-span-2">
-                  <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">关注列表<span
+                  <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">关注账户<span
                       class="text-error-500">*</span></label>
                   <select v-model="selectedFollowId" @change="onFollowChange"
                     class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800">
-                    <option value="">请选择关注列表</option>
+                    <option value="">请选择关注账户</option>
                     <option v-for="follow in followOptions" :key="follow.id" :value="follow.id">{{ follow.external_user_id }}</option>
                   </select>
                 </div>
